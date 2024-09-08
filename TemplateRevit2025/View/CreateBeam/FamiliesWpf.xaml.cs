d7aa19c1ff464c6d678f1f1e2d4c6e86875e0eeb
Nguyen van Phong <ngvanphong2012@gmail.com>
2024-09-08 17:21:09 +0700

    5225255

---
+++ TemplateRevit2025/View/CreateBeam/FamiliesWpf.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TemplateRevit2025.View.CreateBeam
+{
+    /// <summary>
+    /// Interaction logic for FamiliesWpf.xaml
+    /// </summary>
+    public partial class FamiliesWpf : System.Windows.Controls.UserControl
+    {
+        public FamiliesWpf()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TemplateRevit2025/ViewModel/CreateBeam/FamilyVModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TemplateRevit2025.Utilities;
+using Autodesk.Revit.DB;
+
+namespace TemplateRevit2025.ViewModel.CreateBeam
+{
+    class FamilyVModel : ViewModelBase
+    {
+        private readonly Family _family;
+        public long Id
+        {
+            get { return _family.Id.Value; }
+        }
+        public string Name { get; set; }    
+    }
+}
