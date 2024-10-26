8c72ac798da610efd5313e752be29eceb7b455d3
Nguyen van Phong <ngvanphong2012@gmail.com>
2024-10-26 17:03:51 +0700

    52525

--- TemplateRevit2025/Commands/TestCommand.cs
+++ TemplateRevit2025/Commands/TestCommand.cs
@@ -1,8 +1,8 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
-using System.Numerics;
 using System.Security.Cryptography;
 using TemplateRevit2025.Interfaces;
 using TemplateRevit2025.Model.Test;
@@ -26,32 +26,79 @@ namespace TemplateRevit2025.Commands
             ElementId id = ids.First();
 
             FamilyInstance family = doc.GetElement(id) as FamilyInstance;
-
-            
-     
-
-            Location locationFa= family.Location;
-            LocationPoint locationPoint = locationFa as LocationPoint;
-            XYZ pointLo = locationPoint.Point;
-
-
-
-            Transform transformNew = Transform.Identity;
-            transformNew.Origin = pointLo;
-            transformNew.BasisX = new XYZ(0, 1, 0);
-            transformNew.BasisY = new XYZ(-1, 0, 0);
-            transformNew.BasisZ = transformNew.BasisX.CrossProduct(transformNew.BasisY).Normalize();
-
-            using (Transaction t= new Transaction(doc, "Rotaion"))
+            if (family != null)
             {
-                t.Start();
-                Line lineAxis = Line.CreateUnbound(pointLo, XYZ.BasisZ);
-                ElementTransformUtils.RotateElement(doc, family.Id, lineAxis, Math.PI / 2);
-                t.Commit();
+                Location locationFa = family.Location;
+                LocationPoint locationPoint = locationFa as LocationPoint;
+                XYZ pointLo = locationPoint.Point;
+
+
+
+                Transform transformNew = Transform.Identity;
+                transformNew.Origin = pointLo;
+                transformNew.BasisX = new XYZ(0, 1, 0);
+                transformNew.BasisY = new XYZ(-1, 0, 0);
+                transformNew.BasisZ = transformNew.BasisX.CrossProduct(transformNew.BasisY).Normalize();
+
+                using (Transaction t = new Transaction(doc, "Rotaion"))
+                {
+                    t.Start();
+                    Line lineAxis = Line.CreateUnbound(pointLo, XYZ.BasisZ);
+                    ElementTransformUtils.RotateElement(doc, family.Id, lineAxis, Math.PI / 2);
+                    XYZ vector = new XYZ(500 / 304.8, 0, 0);
+                    ElementTransformUtils.MoveElement(doc, family.Id, vector);
+
+                    Plane plane = Plane.CreateByNormalAndOrigin(XYZ.BasisX, pointLo);
+                    ElementTransformUtils.MirrorElements(doc, new List<ElementId> { family.Id }, plane, false);
+                    //ElementTransformUtils.MirrorElement(doc,  family.Id , plane);
+                    //doc.Delete(family.Id);
+
+
+                    t.Commit();
+                }
+                //Transform transformRo = family.GetTransform();
             }
 
-            Transform transformRo = family.GetTransform();
 
+
+
+
+
+            Pipe pipe = doc.GetElement(id) as Pipe;
+
+            LocationCurve locationPipe = pipe.Location as LocationCurve;
+            Line linePipe = locationPipe.Curve as Line;
+
+            double startPa = linePipe.GetEndParameter(0);
+            double endPara = linePipe.GetEndParameter(1);
+            double midPara = (startPa + endPara) / 2;
+            XYZ centerPoint = linePipe.Evaluate(midPara, false);
+
+
+            Transform transformRota = Transform.CreateRotationAtPoint(XYZ.BasisZ, Math.PI / 4, centerPoint);
+            Line rotationLine = linePipe.CreateTransformed(transformRota) as Line;
+            double startParaRo = rotationLine.GetEndParameter(0);
+            double endParaRo = rotationLine.GetEndParameter(1);
+
+            XYZ moveVector = new XYZ(100/304.8, 0, 0);
+            Transform traslate = Transform.CreateTranslation(moveVector);
+            Line lineMove = rotationLine.CreateTransformed(traslate) as Line;
+
+            //Transform reflectionTransform= Transform.CreateReflection()
+
+            Transform transformRo2 = Transform.CreateRotation(XYZ.BasisZ, Math.PI);
+
+            double extend = 500 / 304.8;
+            double startParaExtend = startParaRo - extend / rotationLine.Length * (endParaRo - startParaRo);
+            double endParaExtend = endParaRo + extend / rotationLine.Length * (endParaRo - startParaRo);
+            rotationLine.MakeBound(startParaExtend, endParaExtend);
+
+            using(Transaction t= new Transaction(doc, "ModifyPipe"))
+            {
+                t.Start();
+                locationPipe.Curve = rotationLine;
+                t.Commit();
+            }
 
 
             return Result.Succeeded;
