9856525811b2628f7a14a5db3564364e79b543da
Nguyen van Phong <ngvanphong2012@gmail.com>
2024-11-09 08:51:15 +0700

    5252525

--- TemplateRevit2025/Commands/TestCommand.cs
+++ TemplateRevit2025/Commands/TestCommand.cs
@@ -1,20 +1,6 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
-using Autodesk.Revit.DB.Mechanical;
-using Autodesk.Revit.DB.Plumbing;
 using Autodesk.Revit.UI;
-using Autodesk.Revit.UI.Selection;
-using Autodesk.Windows;
-using Microsoft.Extensions.Logging;
-using System.Configuration;
-using System.Data;
-using System.Security.Cryptography;
-using TemplateRevit2025.Interfaces;
-using TemplateRevit2025.Model.Test;
-using TemplateRevit2025.RevitHandler.Test;
-using TemplateRevit2025.Utilities;
-using TemplateRevit2025.View.Test;
-using TemplateRevit2025.ViewModel.Test;
 
 namespace TemplateRevit2025.Commands
 {
@@ -25,6 +11,8 @@ namespace TemplateRevit2025.Commands
         {
             UIDocument uiDoc = commandData.Application.ActiveUIDocument;
             Document doc = uiDoc.Document;
+
+
 
 
 
