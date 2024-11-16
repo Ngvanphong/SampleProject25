dbaeeabbf4aad27b7b9a24b84d27951f4a0c9797
Nguyen van Phong <ngvanphong2012@gmail.com>
2024-11-16 15:52:55 +0700

    52525

--- TemplateRevit2025/Commands/TestCommand.cs
+++ TemplateRevit2025/Commands/TestCommand.cs
@@ -1,8 +1,9 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
-using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
-using System.Configuration;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.DB.Electrical;
 
 namespace TemplateRevit2025.Commands
 {
@@ -14,87 +15,77 @@ namespace TemplateRevit2025.Commands
             UIDocument uiDoc = commandData.Application.ActiveUIDocument;
             Document doc = uiDoc.Document;
 
-            //Room room = null;
-            //try
-            //{
-            //    var refRoom = uiDoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element, "Pick room");
-            //    room = doc.GetElement(refRoom) as Room;
-            //}
-            //catch { }
+            /*
+             * Duct
+             */
 
-            //SpatialElementBoundaryOptions roomOption = new SpatialElementBoundaryOptions();
-            //roomOption.SpatialElementBoundaryLocation = SpatialElementBoundaryLocation.Finish;
-            //IList<IList<BoundarySegment>> roomBoundarySegment = room.GetBoundarySegments(roomOption);
+            // ve duct
 
-            //List<CurveLoop> listCurveloop = new List<CurveLoop>();
-            //List<Element> listHostELement = new List<Element>();
-            //foreach(IList<BoundarySegment> listSegment in roomBoundarySegment)
-            //{
-            //    CurveLoop curveloopListSemgent = new CurveLoop();
-            //    foreach(BoundarySegment segment in listSegment)
-            //    {
-            //        Element hostOfSegment = doc.GetElement(segment.ElementId);
-            //        listHostELement.Add(hostOfSegment);
-            //        Curve curveOfSegment = segment.GetCurve();
-            //        curveloopListSemgent.Append(curveOfSegment);
-            //    }
-            //    listCurveloop.Add(curveloopListSemgent);
-            //}
+            // duct type
+            IEnumerable<DuctType> listDuctType = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_DuctCurves)
+                 .WhereElementIsElementType().OfClass(typeof(DuctType)).Cast<DuctType>();
 
-            //FloorType typeFloor = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Floors)
-            //    .WhereElementIsElementType().OfClass(typeof(FloorType)).Cast<FloorType>().First();
+            IEnumerable<DuctType> listDuctTypeV = listDuctType.Where(x => x.Shape == ConnectorProfileType.Rectangular);
 
-            //Level activeLevel = doc.ActiveView.GenLevel;
+            IEnumerable<DuctType> listDuctTpeT= listDuctType.Where(x => x.Shape == ConnectorProfileType.Round);
 
-            //using(Transaction t= new Transaction(doc, "CreateFloor"))
-            //{
-            //    t.Start();
-            //    Floor.Create(doc, listCurveloop, typeFloor.Id, activeLevel.Id);
-            //    t.Commit();
-            //}
+            DuctType ductTypeV = listDuctTypeV.FirstOrDefault();
 
-            Toposolid topoSolid = null;
-            try
+            Duct duct1 = null;
+            ConnectorManager connectorManager = duct1.ConnectorManager;
+            ConnectorSet connectorSet = connectorManager.Connectors;
+
+            Connector startConnector = null;
+            Connector endConnector = null;
+            foreach(Connector connector in connectorSet)
             {
-                var refRoom = uiDoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element, "Pick room");
-                topoSolid = doc.GetElement(refRoom) as Toposolid;
-            }
-            catch { }
-
-            SlabShapeEditor slabShapeEditor = topoSolid.GetSlabShapeEditor();
-            SlabShapeVertexArray vertexArray = slabShapeEditor.SlabShapeVertices;
-
-            List<(int indexVertext,SlabShapeVertex)> listVextexInterior = new List<(int,SlabShapeVertex)>();
-            int index = 0;
-            foreach(SlabShapeVertex vertex in vertexArray)
-            {
-                if(vertex.VertexType== SlabShapeVertexType.Interior)
-                {
-                    listVextexInterior.Add((index,vertex));
-                }
-                index++;
+                if (startConnector == null) startConnector = connector;
+                else endConnector = connector;
             }
 
-            using (Transaction t = new Transaction(doc, "ModifyTopo"))
+            IEnumerable<MEPSystemType> listMEPSystemType = new FilteredElementCollector(doc).OfClass(typeof(MEPSystemType))
+               .Cast<MEPSystemType>();
+           
+
+            MEPSystemType returnAir = listMEPSystemType.FirstOrDefault(x=>x.SystemClassification == MEPSystemClassification.ReturnAir);
+
+            IEnumerable<MechanicalSystemType> ductSystems= new FilteredElementCollector(doc).OfClass(typeof(MechanicalSystemType))
+               .Cast<MechanicalSystemType>();
+            MechanicalSystemType ductReturnAir = ductSystems.FirstOrDefault(x => x.SystemClassification == MEPSystemClassification.ReturnAir);
+
+            IEnumerable<PipingSystemType> pipingSystems = new FilteredElementCollector(doc).OfClass(typeof(PipingSystemType))
+               .Cast<PipingSystemType>();
+            PipingSystemType pipeSanitary = pipingSystems.FirstOrDefault(x => x.SystemClassification == MEPSystemClassification.Sanitary);
+
+
+            IEnumerable<MEPSystem> listMepSystem = new FilteredElementCollector(doc).OfClass(typeof(MEPSystem))
+               .Cast<MEPSystem>(); // he thong system ket noi voi nhau
+
+            //Duct newDuct = Duct.Create(doc, ductTypeV.Id, doc.ActiveView.GenLevel.Id,);
+            //Pipe newPipe= Pipe.Create(doc, )
+
+            // duct fitting
+
+            //doc.Create.NewFlexDuct()
+            //doc.Create.NewFamilyInstance()
+
+
+            //Wire newWrite = Wire.Create(doc, ElementId.InvalidElementId, doc.ActiveView, WiringType.Arc,);
+
+            //CableTray cableTrace = CableTray.Create();
+
+
+            CableTray cableTray = null;
+            ConnectorManager cableTrayConnectorManager = cableTray.ConnectorManager;
+            ConnectorSet cableTrayConnectorSet = cableTrayConnectorManager.Connectors;
+            foreach(Connector connector in cableTrayConnectorSet)
             {
-                t.Start();
-                slabShapeEditor.Enable();
-                foreach(var vertexItemPair in listVextexInterior)
-                {
-                    XYZ position = vertexItemPair.Item2.Position;
 
-                    //slabShapeEditor.SlabShapeVertices.set_Item(vertexItemPair.indexVertext,)
-                    slabShapeEditor.ModifySubElement(vertexItemPair.Item2,vertexItemPair.Item2.Position.Z + 4000 / 304.8);
-                    //slabShapeEditor.SlabShapeVertices.set_Item()
-                    slabShapeEditor.AddPoint(position.Add(new XYZ(1000 / 304.8, 0, 4000 / 304.8)));
-                    //slabShapeEditor.AddSplitLine()
-                    
-
-                }
-                
-
-                t.Commit();
             }
+
+            //ConnectorElement connectorElement= ConnectorElement.CreateConduitConnector()
+
+
 
 
 
